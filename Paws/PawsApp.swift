ba6c973334e5a44fcbef250f4e9bf3782fde289b
Josue Lubaki <josuelubaki@gmail.com>
2025-08-16 18:39:47 -0400

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Josue Lubaki on 2025-08-16.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
