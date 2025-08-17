cde8aacba95ebbe7f63f49a8022dce9cb0d27a4d
Josue Lubaki <josuelubaki@gmail.com>
2025-08-16 20:35:32 -0400

    ✨ (feat) Deleting data
    - Add Menu for deletion

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @Query private var pets : [Pet]
     
     @State private var path = [Pet]()
+    @State private var isEditing : Bool = false
     
     let layout = [
         GridItem(.flexible(minimum: 120)),
@@ -21,8 +22,10 @@ struct ContentView: View {
     ]
     
     func addPet() {
+        isEditing = false
         let pet = Pet(name : "Best Friend")
         modelContext.insert(pet)
+        try? modelContext.save()
         path = [pet]
     }
     
@@ -66,6 +69,27 @@ struct ContentView: View {
                             )
                             .background(.ultraThinMaterial)
                             .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            .overlay(alignment: .topTrailing) {
+                                if isEditing {
+                                    Menu {
+                                        Button("Delete", systemImage: "trash", role: .destructive){
+                                            withAnimation {
+                                                modelContext.delete(pet)
+                                                try? modelContext.save()
+                                            }
+                                        }
+                                        
+                                    } label : {
+                                        Image(systemName : "trash.circle.fill")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 36, height: 36)
+                                            .foregroundStyle(.red)
+                                            .symbolRenderingMode(.multicolor)
+                                            .padding()
+                                    }
+                                }
+                            }
                         } //: NavigationLink
                         .foregroundStyle(.primary)
                      } //: Loop
@@ -76,6 +100,16 @@ struct ContentView: View {
             .navigationTitle(pets.isEmpty ? "" : "Paws")
             .navigationDestination(for: Pet.self, destination: EditPetView.init)
             .toolbar {
+                ToolbarItem(placement: .topBarLeading){
+                    Button {
+                        withAnimation {
+                            isEditing.toggle()
+                        }
+                    } label : {
+                        Image(systemName : "slider.horizontal.3")
+                    }
+                }
+
                 ToolbarItem(placement : .topBarTrailing){
                     Button("Add a New Pet", systemImage : "plus.circle", action : addPet)
                 }
